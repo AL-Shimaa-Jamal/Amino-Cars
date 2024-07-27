14387608b4c8c8729ad28b88461d46c2c34bc0ea
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-07-27 14:17:16 +0300

    make it responsive

--- templet1/src/views/NewCars.vue
+++ templet1/src/views/NewCars.vue
@@ -1,163 +1,180 @@
 <template>
-    <div class="main text-capitalize bg-dark position-relative overflow-hidden text-light">
-      <div class="title text-center pt-4">
-        <h1>our new collection</h1>
-        <span>
-          <img src="/Star 3.svg" alt="star" />
-        </span>
-      </div>
-      <!-- new cars images  -->
-      <section class="d-flex justify-content-center align-items-center mt-5 p-2">
-        <!--  cars images -->
-        <div class=" d-flex flex-wrap container justify-content-center ">
-          <!-- images cards -->
-          <div v-for="(card, i) in cars" :key="i" class="card shadow  w-25 bg-dark text-light  ">
-            <div class="card-image  ">
-              <img :src="card.image" alt="cars" />
-            </div>
-            <div class="card-body ">
-              <h4>{{ card.title }}</h4>
-              <p>{{ card.description }}</p>
-              <div class="text-end">{{ card.price }}</div>
-              <div><button class="btn btn-dark text-light text-capitalize shadow btn-outline-light ">buy now</button></div>
+  <div class="main text-capitalize bg-dark position-relative overflow-hidden text-light">
+    <div class="title text-center pt-4">
+      <h1>our new collection</h1>
+      <span>
+        <img src="/Star 3.svg" alt="star" />
+      </span>
+    </div>
+    <!-- new cars images -->
+    <section class="container mt-5 p-2">
+      <!-- cars images -->
+      <div class="card-grid">
+        <!-- images cards -->
+        <div v-for="(card, i) in cars" :key="i" class="card shadow bg-dark text-light">
+          <div class="card-image">
+            <img :src="card.image" alt="cars" />
+          </div>
+          <div class="card-body">
+            <h4>{{ card.title }}</h4>
+            <p>{{ card.description }}</p>
+            <div class="text-end">{{ card.price }}</div>
+            <div>
+              <button class="btn btn-dark text-light text-capitalize shadow btn-outline-light">buy now</button>
             </div>
           </div>
-       </div>
-      </section>
-    </div>
-  </template>
-  
-    
-  <script>
-  export default {
-    data() {
-      return {
-        cars: [
-          {
-            id:1,
-            image: '/new (1).svg',
-            title: 'audi',
-            description: 'now you can do upgrade to you engine',
-            price: '4.500.00 EG'
-          },
-          {
-            id:2,
-            image: '/new (2).svg',
-            title: 'Ferrari',
-            description: 'now you can do upgrade to you engine',
-            price: '2.700.00 eg'
-          },
-          {
-            id:3,
-            image: '/new (3).svg',
-            title: 'Corvete',
-            description: 'now you can do upgrade to you engine',
-            price: '3.650.00 EG'
-          },
-          {
-            id:4,
-            image: "/new (4).svg",
-            title: 'camaro',
-            description: 'now you can do upgrade to you engine',
-            price: '2.400.00 EG'
-          },
-          {
-            id:5,
-            image: "/car1.svg",
-            title: 'camaro',
-            description: 'now you can do upgrade to you engine',
-            price: '2.400.00 EG'
-          },
-          {
-            id:6,
-            image: '/car2.svg',
-            title: 'camaro',
-            description: 'now you can do upgrade to you engine',
-            price: '2.400.00 EG'
-          },
-        ]
-      }
-    },
-    methods: {}
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cars: [
+        {
+          id: 1,
+          image: '/new (1).svg',
+          title: 'PGU 508',
+          description: 'now you can do upgrade to you engine',
+          price: '4.500.00 EG'
+        },
+        {
+          id: 2,
+          image: '/new (2).svg',
+          title: 'Ferrari',
+          description: 'now you can do upgrade to you engine',
+          price: '2.700.00 eg'
+        },
+        {
+          id: 3,
+          image: '/new (3).svg',
+          title: 'BMW',
+          description: 'now you can do upgrade to you engine',
+          price: '3.650.00 EG'
+        },
+        {
+          id: 4,
+          image: "/new (4).svg",
+          title: 'hyndai cn7',
+          description: 'now you can do upgrade to you engine',
+          price: '2.400.00 EG'
+        },
+        {
+          id: 5,
+          image: "/car1.svg",
+          title: 'audi',
+          description: 'now you can do upgrade to you engine',
+          price: '2.600.00 EG'
+        },
+        {
+          id: 6,
+          image: '/car2.svg',
+          title: 'ferrari',
+          description: 'now you can do upgrade to you engine',
+          price: '3.600.00 EG'
+        },
+        {
+          id: 7,
+          image: '/car3.svg',
+          title: 'corvete',
+          description: 'now you can do upgrade to you engine',
+          price: '2.670.00 EG'
+        },
+        {
+          id: 8,
+          image: '/car4.svg',
+          title: 'camaro',
+          description: 'now you can do upgrade to you engine',
+          price: '4.460.00 EG'
+        },
+      ]
+    }
   }
-  </script>
-  
-  <style scoped>
-  .main {
-    height: 200vh;
-    text-transform: capitalize;
-  }
-  
-  .title img {
-    width: 20px;
-    transform: translate(-30%, 80%);
-  }
-  
+}
+</script>
+
+<style scoped>
+.main {
+  height: 100%;
+  text-transform: capitalize;
+}
+
+.title img {
+  width: 20px;
+  transform: translate(-30%, 80%);
+}
+
+h1::after {
+  content: '';
+  width: 11%;
+  background-color: white;
+  height: 1px;
+  position: absolute;
+  top: 8%;
+  right: 52%;
+}
+
+h1::before {
+  content: '';
+  width: 11%;
+  background-color: white;
+  height: 1px;
+  position: absolute;
+  top: 8%;
+  right: 37.9%;
+}
+
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 30px;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  transition: all 0.4s linear;
+}
+
+.card-image img {
+  width: 100%;
+  height: auto;
+}
+
+.card:hover {
+  cursor: pointer;
+  box-shadow: 2px 2px 10px rgb(170, 170, 170) !important;
+  border: 1px solid gray;
+}
+
+.btn:hover {
+  color: black !important;
+}
+
+@media screen and (max-width: 1000px) {
   h1::after {
-    content: '';
-    width: 11%;
-    background-color: white;
-    height: 1px;
-    position: absolute;
-    top: 8%;
-    right: 52%;
+    top: 7.4%;
+    right: 53%;
   }
-  
+
   h1::before {
-    content: '';
-    width: 11%;
-    background-color: white;
-    height: 1px;
-    position: absolute;
-    top: 8%;
-    right: 37.9%;
-  }  
-  .card {
+    top: 7.4%;
+    right: 37.5%;
+  }
+}
 
-    margin: 0 0 20px 20px;
-    transition: all 0.4s linear;
+@media screen and (max-width: 800px) {
+  h1::after {
+    top: 7.5%;
+    right: 54%;
   }
-  .card-image{
-    width:100%;
 
+  h1::before {
+    top: 7.5%;
+    right: 37.5%;
   }
-  .card-image img{
-width: 100%;
-  }
-  .card:hover {
-    cursor: pointer;
-    box-shadow: 2px 2px 10px rgb(170, 170, 170) !important;
-    margin: 2%;
-    border: 1px solid gray;
-  }
-  .btn:hover{
-color:black !important;
-  }
-  @media screen and (max-width: 1000px) {
-    h1::after {
-      top: 7.4%;
-      right: 53%;
-    }
-  
-    h1::before {
-      top: 7.4%;
-      right: 37.5%;
-    }
-  }
-  
-  @media screen and (max-width: 800px) {
-    h1::after {
-      top: 7.5%;
-      right: 54%;
-    }
-  
-    h1::before {
-      top: 7.5%;
-      right: 37.5%;
-    }
-    .card:hover {
-    margin: 0;
-  }
-  }
-  </style>
-  
+}
+</style>
