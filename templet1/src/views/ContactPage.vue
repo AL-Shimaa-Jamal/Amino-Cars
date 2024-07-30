1438394aa33b5a79e7c01e5892e460b5bb85bdcf
ALshimaa jamal <143935202+AL-Shimaa-Jamal@users.noreply.github.com>
2024-07-30 15:26:11 +0300

    update cards in this component and try to create a carousel

--- templet1/src/views/ContactPage.vue
+++ templet1/src/views/ContactPage.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="main text-capitalize bg-dark position-relative overflow-hidden text-light">
     <div class="title text-center pt-4">
-    <h1 class="text-center p-4">what our customer have to say !!
-      <span>
-        <img src="/Star 3.svg" alt="star" />
-      </span>
-    </h1>
-    <p class="container w-25 pt-5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor blanditiis saepe id hic consequuntur tenetur molestias praesentium sunt perferendis nulla nisi ex, suscipit eaque, natus optio culpa debitis. Officia?
-    </p>
+      <h1 class="text-center p-4">
+        what our customer have to say !!
+        <span>
+          <img src="/Star 3.svg" alt="star" />
+        </span>
+      </h1>
+      <p class="container w-25 pt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor blanditiis saepe id hic consequuntur tenetur molestias praesentium sunt perferendis nulla nisi ex, suscipit eaque, natus optio culpa debitis. Officia?
+      </p>
     </div>
     <!-- new cars images -->
     <section class="container mt-5 p-2">
       <!-- cars images -->
       <div class="card-grid">
         <!-- images cards -->
-        <div v-for="(card, i) in cars" :key="i" class="card shadow bg-dark text-light">
+        <div v-for="(card, i) in cars" :key="i" class="card shadow bg-dark text-light" :class="{ active: activeIndex === i }">
           <div class="card-image">
             <img :src="card.image" alt="cars" />
           </div>
@@ -23,22 +24,16 @@
             <h4>{{ card.title }}</h4>
             <div class="text-end">{{ card.rate }}</div>
             <p>{{ card.description }}</p>
-            <div>
-              <button class="btn btn-dark text-light text-capitalize shadow btn-outline-light">buy now</button>
-            </div>
           </div>
         </div>
       </div>
-      <div class="spbtn container text-center m-3">
-       
-          <span class="m-3">.</span>
-          <span class="m-3">.</span>
-          <span class="m-3">.</span>
-     
+      <div class="spbtn container text-center m-3 list-unstyled d-flex justify-content-center align-items-center">
+        <li v-for="(card, i) in cars" :key="i" @click="setActive(i)" :class="{ active: activeIndex === i }" class="m-3"></li>
       </div>
     </section>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -50,28 +45,31 @@ export default {
           image: '/new (1).svg',
           title: 'mohammed',
           description: 'now you can do upgrade to you engine',
-          
         },
         {
           id: 2,
           image: '/new (2).svg',
           title: 'gamila 014',
           description: 'now you can do upgrade to you engine',
-          
         },
         {
           id: 3,
           image: '/new (3).svg',
           title: 'kareem',
           description: 'now you can do upgrade to you engine',
-        
         },
-      
-      ]
-    }
-  }
-}
+      ],
+      activeIndex: 1,
+    };
+  },
+  methods: {
+    setActive(index) {
+      this.activeIndex = index;
+    },
+  },
+};
 </script>
+
 
 <style scoped>
 .main {
@@ -126,19 +124,23 @@ h1::before {
   width: 100%;
   height: auto;
 }
-
-.card:hover {
+.card.active {
   cursor: pointer;
   box-shadow: 2px 2px 10px rgb(170, 170, 170) !important;
   border: 1px solid gray;
+  transform: scale(1.1,1.1);
 }
 
-.spbtn span{
-width: 150px !important;
-height: 150px !important;
-border-radius: 50%;
-background-color: #fff;
+.spbtn li {
+  width: 10px !important;
+  height: 10px !important;
+  border-radius: 50%;
+  background-color: #fff;
+  cursor: pointer;
+}
 
+.spbtn li.active {
+  background-color: blue !important;
 }
 
 @media screen and (max-width: 1200px) {
@@ -157,3 +159,4 @@ background-color: #fff;
   }
 }
 </style>
+
